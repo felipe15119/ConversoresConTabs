14b0d2ed51fbd18732787b83a267a4631237394e
Felipe Martinez <Felipe151199/Programacion>
2020-08-19 15:08:08 -0600

    Correccion De Errores en Lineas de Codigos y Diseño

--- app/src/main/java/com/ugb/conversores/MainActivity.java
+++ app/src/main/java/com/ugb/conversores/MainActivity.java
@@ -19,13 +19,14 @@ public class MainActivity extends AppCompatActivity {
         tbhConversores.setup();
 
         ///////
+        // tbhConversores.addTab(tbhConversores.newTabSpec("M").setContent(R.id.tabMonedas).setIndicator("Monedas"));
         tbhConversores.addTab(tbhConversores.newTabSpec("L").setContent(R.id.tabLongitud).setIndicator("Longuitud"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("M").setContent(R.id.tabMasa).setIndicator("Masa"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("V").setContent(R.id.tabVolumen).setIndicator("Volumen"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("S").setContent(R.id.tabAlmacenamiento).setIndicator("Almacenamiento"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("T").setContent(R.id.tabTranf).setIndicator("Transferencia"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("M").setContent(R.id.tabMonedas).setIndicator("Monedas"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("MA").setContent(R.id.tabMasa).setIndicator("Masa"));
         tbhConversores.addTab(tbhConversores.newTabSpec("T").setContent(R.id.tabTiempo).setIndicator("Tiempo"));
+       // tbhConversores.addTab(tbhConversores.newTabSpec("V").setContent(R.id.tabVolumen).setIndicator("Volumen"));
+       // tbhConversores.addTab(tbhConversores.newTabSpec("S").setContent(R.id.tabAlmacenamiento).setIndicator("Almacenamiento"));
+       // tbhConversores.addTab(tbhConversores.newTabSpec("TA").setContent(R.id.tabTranf).setIndicator("Transferencia"));
+
     }
     public void calcular(View v){
         TextView tmpVal = (TextView)findViewById(R.id.txtCantidad);
@@ -36,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
                 new double[]{1,0.001,100,1000,1000000,1000000000,0.0006,1.0936,3.2808,39.9701,0.00053996}, //longitud
                 new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16}, //masa
-              new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16},
+              new double[]{1,1000000000,1000000,1000,0.0166666667,0.0002777778,0.0000115741,0.0000016534,0.0000003802570,0.00000003168808,0.000000003169,0.0000000003169} //Tiempo
 
         };
         int de=0, a=0;
@@ -50,12 +51,19 @@ public class MainActivity extends AppCompatActivity {
                 a = spn.getSelectedItemPosition();
                 resp = valores[0][a] / valores[0][de] * cantidad;
                 break;
-            case "Ma":
+            case "MA":
                 spn = (Spinner)findViewById(R.id.cboDema);
                 de = spn.getSelectedItemPosition();
-                spn = (Spinner)findViewById(R.id.cboma);
+                spn = (Spinner)findViewById(R.id.cboAma);
                 a = spn.getSelectedItemPosition();
                 resp = valores[1][a] / valores[1][de] * cantidad;
+                break;
+          case "T":
+              spn = (Spinner)findViewById(R.id.cboDeT);
+                de = spn.getSelectedItemPosition();
+                spn = (Spinner)findViewById(R.id.cboAT);
+                a = spn.getSelectedItemPosition();
+                resp = valores[2][a] / valores[2][de] * cantidad;
                 break;
 
         }

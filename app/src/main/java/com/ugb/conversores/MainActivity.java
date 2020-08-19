37f8f5b4de45a577f5a791cc771c7a26f5f36bb5
AnibalLP <anibalinoz@gmail.com>
2020-08-19 09:30:51 -0600

    se agregaron las pestañas de todo los conversores

--- app/src/main/java/com/ugb/conversores/MainActivity.java
+++ app/src/main/java/com/ugb/conversores/MainActivity.java
@@ -20,7 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         ///////
         tbhConversores.addTab(tbhConversores.newTabSpec("L").setContent(R.id.tabLongitud).setIndicator("Longuitud"));
-        tbhConversores.addTab(tbhConversores.newTabSpec("Ma").setContent(R.id.tabMasa).setIndicator("Masa"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("M").setContent(R.id.tabMasa).setIndicator("Masa"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("V").setContent(R.id.tabVolumen).setIndicator("Volumen"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("S").setContent(R.id.tabAlmacenamiento).setIndicator("Almacenamiento"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("T").setContent(R.id.tabTranf).setIndicator("Transferencia"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("M").setContent(R.id.tabMonedas).setIndicator("Monedas"));
+        tbhConversores.addTab(tbhConversores.newTabSpec("T").setContent(R.id.tabTiempo).setIndicator("Tiempo"));
     }
     public void calcular(View v){
         TextView tmpVal = (TextView)findViewById(R.id.txtCantidad);
@@ -30,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         double valores[][] = {
 
                 new double[]{1,0.001,100,1000,1000000,1000000000,0.0006,1.0936,3.2808,39.9701,0.00053996}, //longitud
-                new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16} //masa
+                new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16}, //masa
+              new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16},
+
         };
         int de=0, a=0;
         double resp = 0;
@@ -46,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             case "Ma":
                 spn = (Spinner)findViewById(R.id.cboDema);
                 de = spn.getSelectedItemPosition();
-                spn = (Spinner)findViewById(R.id.cboAma);
+                spn = (Spinner)findViewById(R.id.cboma);
                 a = spn.getSelectedItemPosition();
                 resp = valores[1][a] / valores[1][de] * cantidad;
                 break;

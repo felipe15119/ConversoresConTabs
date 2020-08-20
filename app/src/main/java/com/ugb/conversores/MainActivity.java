402e8159f8b51af125b894865e950a2b9b4c5cf0
Felipe Martinez <Felipe151199/Programacion>
2020-08-19 20:28:41 -0600

    Integracion de Convertidor Transferencia de Datos

--- app/src/main/java/com/ugb/conversores/MainActivity.java
+++ app/src/main/java/com/ugb/conversores/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tbhConversores.addTab(tbhConversores.newTabSpec("T").setContent(R.id.tabTiempo).setIndicator("Tiempo"));
         tbhConversores.addTab(tbhConversores.newTabSpec("A").setContent(R.id.tabAlmacenamiento).setIndicator("Almacenamiento"));
         tbhConversores.addTab(tbhConversores.newTabSpec("V").setContent(R.id.tabVolumen).setIndicator("Volumen"));
-       // tbhConversores.addTab(tbhConversores.newTabSpec("TA").setContent(R.id.tabTranf).setIndicator("Transferencia"));
+       tbhConversores.addTab(tbhConversores.newTabSpec("TD").setContent(R.id.tabTrasnferenciaDatos).setIndicator("Transferencia"));
 
     }
     public void calcular(View v){
@@ -39,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
                 new double[]{1,0.00045359,0.4535923,453.5923,453592.37,0.00000004535923,0.0004464286,0.0005,0.0089285714,0.01,16}, //masa
                 new double[]{1,1000000000,1000000,1000,0.0166666667,0.0002777778,0.0000115741,0.0000016534,0.0000003802570,0.00000003168808,0.000000003169,0.0000000003169}, //Tiempo
                 new double[]{1,13,0.000977,0.000000954,0.000000000931,0.000000000000909,0.000000000000000888,0.000122,0.000000119,0.000000000116,0.000000000000000111}, //Almacenamiento
-                new double[]{1,4,8,15.7725,128,256,768,0.00378541,3.78541,3785.41,0.832674,3.3307,6.66139,13.3228,133.228,213.165,639.494,0.133681,231} //Volumen...
+                new double[]{1,4,8,15.7725,128,256,768,0.00378541,3.78541,3785.41,0.832674,3.3307,6.66139,13.3228,133.228,213.165,639.494,0.133681,231}, //Volumen...
+                new double[]{1,0.001,0.000125,0.000001,0.000000125,0.000000001,0.000000000125,0.000000000001,0.000000000000125} //Transferencia de Datos
 
         };
         int de=0, a=0;
@@ -87,6 +88,13 @@ public class MainActivity extends AppCompatActivity {
                 spn = (Spinner)findViewById(R.id.cboDeV);
                 de = spn.getSelectedItemPosition();
                 spn = (Spinner)findViewById(R.id.cboAV);
+                a = spn.getSelectedItemPosition();
+                resp = valores[4][a] / valores[4][de] * cantidad;
+                break;
+            case "TD":
+                spn = (Spinner)findViewById(R.id.cboDeTD);
+                de = spn.getSelectedItemPosition();
+                spn = (Spinner)findViewById(R.id.cboATD);
                 a = spn.getSelectedItemPosition();
                 resp = valores[4][a] / valores[4][de] * cantidad;
                 break;
